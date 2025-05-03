d5686748d1b666c4043779ff3b3efb65506949d0
İsmail Başaran <bsrn.ismail@gmail.com>
2025-05-03 21:54:58 +0300

    postgresql integration

--- backend/chaintract/settings.py
+++ backend/chaintract/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'chaintract.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATBASE_PASSWORD'),
+        'HOST': config('DATABSE_HOST'),
+        'PORT': config('DATABSE_PORT'),
     }
 }
 
